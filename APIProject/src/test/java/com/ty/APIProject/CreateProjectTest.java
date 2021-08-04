c6c8d9ead8d8d9542ff92c2b345a0b28f7b5d410
rohit0596 <rohit.raj.0696@gmail.com>
2021-08-04 19:22:03 +0530

    This is evening commit

---
+++ APIProject/src/test/java/com/ty/APIProject/CreateProjectTest.java
@@ -0,0 +1,11 @@
+package com.ty.APIProject;
+
+import org.testng.annotations.Test;
+
+public class CreateProjectTest {
+
+	@Test
+	public void createProjectTest() {
+		System.out.println("Hii.. This method will create project.");
+	}
+}
